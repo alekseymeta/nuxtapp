6942242014af19592b51ff62ab341dacc624283f
Aleksey <74327665+alekseymeta@users.noreply.github.com>
2022-05-17 02:54:42 +0300

    Updates

--- _nuxt/static/1652744996/account/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxtapp\u002F_nuxt\u002Fstatic\u002F1652744996",layout:"default",error:a,state:{counter:0,userData:[],showSpeaner:false,showAlert:[]},serverRendered:true,routePath:"\u002Faccount",config:{_app:{basePath:"\u002Fnuxtapp\u002F",assetsPath:"\u002Fnuxtapp\u002F_nuxt\u002F",cdnURL:a}}}}(null));
